c39b6f824d36e707660297e798fd46bb84e0bacc
salehkaddoura <saleh.kaddoura@gmail.com>
2015-11-07 23:59:31 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var port = process.env.PORT || 8080;
+
+app.use(morgan('short'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next){
+    console.log('something is happening');
+    next();
+});
+
+app.get('/', function(req, res) {
+    res.status(200).json({ message: "hey" });
+});
+
+app.listen(port);
+console.log("The magic happens on port: ", port);
